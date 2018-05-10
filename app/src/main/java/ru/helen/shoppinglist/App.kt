7c056de2bbb132464c6fb641eeb8deb3018d3116
lenap <pogorelets.elena@mail.ru>
2018-05-10 18:54:05 +0300

    behavir для кнопеи добавать, dialogfragment

--- app/src/main/java/ru/helen/shoppinglist/App.kt
+++ app/src/main/java/ru/helen/shoppinglist/App.kt
@@ -1,6 +1,7 @@
 package ru.helen.shoppinglist
 
 import android.app.Application
+import android.support.v7.app.AppCompatDelegate
 import ru.helen.shoppinglist.di.AppComponent
 import ru.helen.shoppinglist.di.AppModule
 import ru.helen.shoppinglist.di.DaggerAppComponent
@@ -16,6 +17,7 @@ class App : Application() {
 
     override fun onCreate() {
         super.onCreate()
+        AppCompatDelegate.setCompatVectorFromResourcesEnabled(true)
         instance = this
         appComponent = DaggerAppComponent
                 .builder()
--- app/src/main/java/ru/helen/shoppinglist/ui/MainActivity.kt
+++ app/src/main/java/ru/helen/shoppinglist/ui/MainActivity.kt
@@ -1,23 +1,18 @@
 package ru.helen.shoppinglist.ui
-import android.app.Dialog
+
+
+import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProviders
-
 import android.os.Bundle
-
-import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
-import android.arch.lifecycle.Observer
 import android.util.Log
 import io.reactivex.Completable
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.functions.Action
 import io.reactivex.schedulers.Schedulers
-
-
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
-import kotlinx.android.synthetic.main.dialog_create_list.view.*
 import ru.helen.shoppinglist.App
 import ru.helen.shoppinglist.R
 import ru.helen.shoppinglist.entity.Shoppinglist
@@ -27,7 +22,7 @@ import ru.helen.shoppinglist.viewmodel.MainModelFactory
 import java.util.*
 import javax.inject.Inject
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), DialogCreateList.DialogCreateListener {
     lateinit var adapter: MainAdapter
     @Inject
     lateinit var viewModelFactory: MainModelFactory
@@ -46,34 +41,38 @@ class MainActivity : AppCompatActivity() {
                 .of(this, viewModelFactory)
                 .get(MainModel::class.java!!)
 
-        viewModel.getAll().observe(this,Observer {responce -> showLists(responce) })
-        fab.setOnClickListener { dialogInsert().show()}
+        viewModel.getAll().observe(this, Observer { responce -> showLists(responce) })
+        //fab.setOnClickListener { dialogInsert().show()}
+        fab.setOnClickListener {
+            val dialog = DialogCreateList.newInstance()
+            dialog.show(supportFragmentManager, "dialog")
+        }
     }
 
-    fun dialogInsert(): Dialog {
-        val view = this.layoutInflater.inflate(R.layout.dialog_create_list, null)
-        return AlertDialog.Builder(this)
-                .setView(view)
-                .setPositiveButton(getString(R.string.create_list_button), { dialog, which ->insertList(view.nameList.text.toString())})
-                .setNegativeButton(getString(R.string.cancel_button), { dialog, which -> dialog.dismiss() })
-                .create()
+//    fun dialogInsert(): Dialog {
+//        val view = this.layoutInflater.inflate(R.layout.dialog_create_list, null)
+//        return AlertDialog.Builder(this)
+//                .setView(view)
+//                .setPositiveButton(getString(R.string.create_list_button), { dialog, which ->insertList(view.nameList.text.toString())})
+//                .setNegativeButton(getString(R.string.cancel_button), { dialog, which -> dialog.dismiss() })
+//                .create()
+//
+//    }
 
-    }
-
-    fun insertList(nameList: String){
-        Completable.fromAction(Action { viewModel.insert(Shoppinglist(null,nameList,Date())) })
+    override fun insertList(nameList: String) {
+        Completable.fromAction(Action { viewModel.insert(Shoppinglist(null, nameList, Date())) })
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe({},{error -> Log.e("ERROR", error.toString())})
+                .subscribe({}, { error -> Log.e("ERROR", error.toString()) })
 
 
     }
 
-    fun showLists(lists: List<Shoppinglist>?){
+    fun showLists(lists: List<Shoppinglist>?) {
         if (lists != null) {
             adapter.swapData(lists)
         }
-       Log.e("SIZE", lists?.size.toString())
+        Log.e("SIZE", lists?.size.toString())
     }
 
 }
