c3cc2c92d611431371e14cf299d6080f6dedd003
lenap <pogorelets.elena@mail.ru>
2018-05-14 22:53:04 +0300

    добавление SearchView в диалоге добавления покупки

--- app/src/main/java/ru/helen/shoppinglist/ui/product/DialogCreateProduct.kt
+++ app/src/main/java/ru/helen/shoppinglist/ui/product/DialogCreateProduct.kt
@@ -3,6 +3,8 @@ package ru.helen.shoppinglist.ui.product
 import android.app.AlertDialog
 import android.os.Bundle
 import android.support.v4.app.DialogFragment
+import android.util.Log
+import android.widget.SearchView
 import kotlinx.android.synthetic.main.dialog_create_list.view.*
 import kotlinx.android.synthetic.main.dialog_create_product.view.*
 import ru.helen.shoppinglist.R
@@ -23,13 +25,31 @@ class DialogCreateProduct: DialogFragment() {
 
     override fun onCreateDialog(savedInstanceState: Bundle?): AlertDialog {
         val dialogView = activity.layoutInflater.inflate(R.layout.dialog_create_product, null)
-        val nameList = dialogView.nameList
+
+        dialogView.searchProduct.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextChange(newText: String?): Boolean {
+                if (newText != null){
+                   // searchList(newText)
+                }
+
+                return true
+            }
+
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return true
+            }
+
+
+
+        })
+
 
         val dialog = AlertDialog.Builder(activity, R.style.MyDialogTheme)
                 .setView(dialogView)
                 .setPositiveButton(activity.getString(R.string.create_list_button), { dialog, which ->
-                    if (dialogView.nameProduct.text.toString()!= ""){
-                        (activity as ProductListener).insertProduct(dialogView.nameProduct.text.toString())
+                    if (dialogView.searchProduct.query!= ""){
+                        Log.e("query", dialogView.searchProduct.query.toString())
+                        (activity as ProductListener).insertProduct(dialogView.searchProduct.query.toString())
                     }
                 })
 
